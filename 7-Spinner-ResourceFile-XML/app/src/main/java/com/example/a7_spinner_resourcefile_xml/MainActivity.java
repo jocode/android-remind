ef3a3651a39270de32303791dc615e10d3d9d051
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-27 15:49:34 -0500

    Spinner con opciones en un Archivo de recursos
    Se ha creado un Spinner en la interfaz, y se le ha asignado un id para
    vincularlo a la actividad.
    
    Se debe crear un archivo de recursos strind en el directorio res/values,
    en este caso le coloqué por nombre como arrays, en él se define una
    etiqueta llamada string-array y se le coloca un nombre, cada valor se
    define en una etiqueta llamada item.
    
    Luego se asignan las opciones al Spinner con un ArrayAdapter, o
    CursosAdapter si se hace una consulta a la base de datos. Se crea el
    adaptador con el método createFromResourse(), y los argumentos que se le
    pasan son el contexto, el string-array y el diseño del Spinner.
    Luego se especifica el diseño con el método setDropDownResource(int) y
    finalmente de llama el método setAdapter() para aplicar el adaptador al
    Spinner.
    
    Para recibir el evento del Spinner se usa el método
    onItemSelectedListener, que se implementa con la Interfaz
    AdapterView.OnItemSelectedListener, y esta implementación requiere que
    se establezcan los métodos onItemSelected() y onNothingSelected() y lugo
    se implementa se esta manera:
    spinner.setOnItemSelectedListener(this);

--- 7-Spinner-ResourceFile-XML/app/src/main/java/com/example/a7_spinner_resourcefile_xml/MainActivity.java
+++ 7-Spinner-ResourceFile-XML/app/src/main/java/com/example/a7_spinner_resourcefile_xml/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.a7_spinner_resourcefile_xml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spPlanets = (Spinner) findViewById(R.id.spPlanetas);
+
+        //Crear un ArrayAdapter usando el string array y un diseño de spinner por defecto
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+
+        //Especificar el diseño a utilizar cuando aparezca la lista de opciones
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Aplicar el adaptador al Spinner
+        spPlanets.setAdapter(adapter);
+
     }
 }

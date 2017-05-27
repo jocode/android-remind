9215a63380b8e42ba1b9c29b2dd806b695824162
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-27 16:35:14 -0500

    Spinner con opciones de ArrayList
    Otra forma de llenar una lista desplegable, es agregando los valores en
    un ArrayList, para ello he creado un ArrayList llamado lenguajes y he
    colocado unos valores. Luego de definir el array uso un ArrayAdapter y
    le paso por parámetro el contexto, el Diseño y el array para mostrar.
    
    Finalmente aplico el adaptador al Spinner con el método setAdapter();

--- 7-Spinner-ResourceFile-XML/app/src/main/java/com/example/a7_spinner_resourcefile_xml/MainActivity.java
+++ 7-Spinner-ResourceFile-XML/app/src/main/java/com/example/a7_spinner_resourcefile_xml/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.a7_spinner_resourcefile_xml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +16,29 @@ public class MainActivity extends AppCompatActivity {
 
         Spinner spPlanets = (Spinner) findViewById(R.id.spPlanetas);
 
+        ArrayList languages = new ArrayList();
+        languages.add("Java");
+        languages.add("PHP");
+        languages.add("Phyton");
+        languages.add("Kotlin");
+        languages.add("Go");
+        languages.add("JavaScrip");
+        languages.add("C#");
+        languages.add("VB");
+        languages.add("C");
+        languages.add("C++");
+        languages.add("Swift");
+
         //Crear un ArrayAdapter usando el string array y un diseño de spinner por defecto
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
 
         //Especificar el diseño a utilizar cuando aparezca la lista de opciones
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        ArrayAdapter adapter1 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, languages);
+
         //Aplicar el adaptador al Spinner
-        spPlanets.setAdapter(adapter);
+        spPlanets.setAdapter(adapter1);
 
     }
 }

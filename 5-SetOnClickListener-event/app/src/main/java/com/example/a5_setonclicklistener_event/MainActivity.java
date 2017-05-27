a7e2bd1baf323314c9ac16eb3feb4799df39bf04
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-26 21:49:55 -0500

    Evento setOnClickListener
    El evento setOnClickListener, se usa cuando se dac clic en determinados
    elementos de la vista. Lo que hace este evento es estar pendiente cuando
    se de clic sobre un elemento para realizar una acción.
    
    En este caso al dar clic en el botón Saludar, se realiza una acción, que
    es tomar el valor de la caja de texto y colocarla en el TextView.

--- 5-SetOnClickListener-event/app/src/main/java/com/example/a5_setonclicklistener_event/MainActivity.java
+++ 5-SetOnClickListener-event/app/src/main/java/com/example/a5_setonclicklistener_event/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.a5_setonclicklistener_event;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNombre;
+    TextView tvResultado;
+    Button btnSaludar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNombre = (EditText) findViewById(R.id.etNombre);
+        tvResultado = (TextView) findViewById(R.id.tvResultado);
+        btnSaludar = (Button) findViewById(R.id.btnSaludar);
+
+        btnSaludar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvResultado.setText(etNombre.getText().toString());
+            }
+        });
     }
 }

b62abca730a0b6afd3dc72329fbbf5d86c0c8905
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-27 14:51:00 -0500

    CheckBox y RadioButton
    Se ha creado estos elementos en la layout. El RadioButton se coloca
    dentro de otra etiqueta que se llama RadioGroup, ¿Por qué?, porque en
    muchas ocasiones hay varias opciones pero se debe seleccionar solo una,
    y al hacer esto le indicamos a que todos los Radio Buttons son hermanos,
    y solo se puede seleccionar un de ese  tipo que se esté trabajando.
    Los checkbox se definen sin tener una etiqueta que actúe como padre.
    
    Los métodos ussados para ambos componentes es isChecked(), de esta forma
    valida si se ha dado clic sobre alguno de los elementos.
    
    Con el método setText(), definimos el texto a mostrar en el TextView.

--- 6-RadioButton-CheckBox/app/src/main/java/com/example/a6_radiobutton_checkbox/MainActivity.java
+++ 6-RadioButton-CheckBox/app/src/main/java/com/example/a6_radiobutton_checkbox/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.a6_radiobutton_checkbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSeleccion;
+    TextView tvSeleccion;
+    CheckBox cboxTres, cboxCuatro;
+    RadioButton rbUno, rbDos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSeleccion = (Button) findViewById(R.id.btnSeleccion);
+        tvSeleccion = (TextView) findViewById(R.id.tvSeleccion);
+        cboxCuatro = (CheckBox) findViewById(R.id.cboxCuatro);
+        cboxTres = (CheckBox) findViewById(R.id.cboxTres);
+        rbDos = (RadioButton) findViewById(R.id.rbDos);
+        rbUno  = (RadioButton) findViewById(R.id.rbUno);
+
+        btnSeleccion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(rbUno.isChecked()){
+                    tvSeleccion.setText(rbUno.getText());
+                } else if (rbDos.isChecked()){
+                    tvSeleccion.setText(rbDos.getText());
+                } else if(cboxTres.isChecked()){
+                    tvSeleccion.setText(cboxTres.getText());
+                } else if (cboxCuatro.isChecked()){
+                    tvSeleccion.setText(cboxCuatro.getText());
+                } else {
+                    tvSeleccion.setText("No se ha seleccionado nada");
+                }
+
+
+            }
+        });
     }
 }

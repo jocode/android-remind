43244dd11819ede448993919dd384c2570b0ffc4
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-28 16:57:18 -0500

    ListView - Cargar listado desde ArrayList
    Para cargar los datos, que se van a ver en el listado desde un
    ArrayList, desde Java, en lugar de un archivo de recursos, debemos crear
    un ArrayList  y agregar un listado de   elementos. Luego creamos un
    ArrayAdapter y lo asignamos al listView.

--- 8-ListView/app/src/main/java/com/example/a8_listview/MainActivity.java
+++ 8-ListView/app/src/main/java/com/example/a8_listview/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView lvPlanetas;
@@ -16,8 +18,26 @@ public class MainActivity extends AppCompatActivity {
 
         lvPlanetas = (ListView) findViewById(R.id.lvPlanetas);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.list_planets, android.R.layout.simple_list_item_1);
+        ArrayList languajes = new ArrayList();
+        languajes.add("Java");
+        languajes.add("Kotlin");
+        languajes.add("Swift");
+        languajes.add("C#");
+        languajes.add("C++");
+        languajes.add("Javascript");
+        languajes.add("Phyton");
+        languajes.add("Ruby");
+        languajes.add("PHP");
+        languajes.add("C");
+        languajes.add("Objective-C");
+        languajes.add("Perl");
+        languajes.add("Go");
+        languajes.add("Visual Basic");
+        languajes.add("Spark");
+
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, languajes);
+
         lvPlanetas.setAdapter(adapter);
-        
+
     }
 }

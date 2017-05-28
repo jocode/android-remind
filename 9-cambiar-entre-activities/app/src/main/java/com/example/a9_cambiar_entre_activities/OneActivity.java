f2c0d37363089b92019fa3c32e01b581c595a9b0
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-28 17:27:21 -0500

    Intent - Para el cambio de Actividades
    Un intent es una descripción abstracta d una operación a realizar. Al
    usarse con el método startActivity, puede ejecutarse una Activity,
    enviar datos, o llamar a un servicio de Android.
    
    Se declara el intent, y se le pasa por primer parámetro la actividad actual, y
    como segundo parámetro la actividad o servicio al cuál me va a llevar.
    
    Luego de eso, se usa el método startActivity(), y se le pasa por
    parámetro el intent.
    
    El método finish, usado para en el botón salir, me cierra la actividad
    actual. Si lo que quiero es cerrar la aplicación, este método debe
    usarse en la actividad Lanzadora.
    
    Por cada actividad creada, en el archivo manifest, se crea las
    actividades y servicios que se vayan a usar en la aplicación. Aquí
    tambien se dan los permisos que la aplicación usará como son el de
    conexión a internet, o de sensores.

--- 9-cambiar-entre-activities/app/src/main/java/com/example/a9_cambiar_entre_activities/OneActivity.java
+++ 9-cambiar-entre-activities/app/src/main/java/com/example/a9_cambiar_entre_activities/OneActivity.java
@@ -1,7 +1,10 @@
 package com.example.a9_cambiar_entre_activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OneActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class OneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSiguiente = (Button) findViewById(R.id.btnSiguiente);
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(OneActivity.this, TwoActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
--- 9-cambiar-entre-activities/app/src/main/java/com/example/a9_cambiar_entre_activities/TwoActivity.java
+++ 9-cambiar-entre-activities/app/src/main/java/com/example/a9_cambiar_entre_activities/TwoActivity.java
@@ -1,7 +1,10 @@
 package com.example.a9_cambiar_entre_activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TwoActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class TwoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+
+        Button btnSalir = (Button) findViewById(R.id.btnSalir);
+        Button btnVolver = (Button) findViewById(R.id.btnVolver);
+
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(TwoActivity.this, OneActivity.class));
+
+            }
+        });
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+            }
+        });
     }
 }

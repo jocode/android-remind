a380699501d4745eb2713a6f7c6e42658f44c5f1
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-28 17:10:55 -0500

    9 - Cambiar entre Activities (Pantalla)
    Para cambiar entre pantallas, se hace con Intent, para este caso, en el
    diseño de la actividad uno, creo un botón para que me lleve a la
    siguiente pantalla y en la actividad dos voy a tener dos botones, uno
    que me devuelve a la primera actividad y otro que sale de la aplicación.

---
+++ 9-cambiar-entre-activities/app/src/main/java/com/example/a9_cambiar_entre_activities/OneActivity.java
@@ -0,0 +1,13 @@
+package com.example.a9_cambiar_entre_activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ 9-cambiar-entre-activities/app/src/main/java/com/example/a9_cambiar_entre_activities/TwoActivity.java
@@ -0,0 +1,13 @@
+package com.example.a9_cambiar_entre_activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}

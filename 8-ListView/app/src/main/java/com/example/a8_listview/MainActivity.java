1a94fa396bb8e42b18a2879b257dbd783a4d46a4
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-28 16:38:54 -0500

    Listview - Cargar la lista desde resources en xml
    1. Creo el diseño de la aplicación. Para ello coloco un TextView y un ListView, al cuál le asigno un id.
    2. Creoun nuevo archivo llamado arrays.xml en la carpeta values dentro de res.
    3. Dentro del archivo arrays.xml, creo un string-array con el nombre de list_planets, y dentro de él coloco un listado de items, que serán los nombres de los planetas
    4. En el MainActivity, declaro el ListView y hago la instancia del elemento de la vista.
    5. Creo un ArrayAdapter, en el cuál escojo el diseño y los valores del array que he creado en el archivo arrays.xml
    6. Finalmente asigno el adaptador al ListView

--- 8-ListView/app/src/main/java/com/example/a8_listview/MainActivity.java
+++ 8-ListView/app/src/main/java/com/example/a8_listview/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.a8_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvPlanetas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvPlanetas = (ListView) findViewById(R.id.lvPlanetas);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.list_planets, android.R.layout.simple_list_item_1);
+        lvPlanetas.setAdapter(adapter);
+        
     }
 }

25ae69afb93f92b55cf3a5afc394a2891e9d697d
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-29 17:03:42 -0500

    12 - Fragment Estáticos
    Un Fragment representa un comportamiento o una parte de la interfaz de
    usuario en una Activity. Puedes combinar múltiples fragmentos en una
    sola actividad para crear una IU multipanel y volver a usar un fragmento
    en múltiples actividades.
    
    En otras palabras son secciones modulares, o subactividades que puedes
    usar en diferente actividades.

---
+++ 12-fragment-staticos/app/src/main/java/com/example/a12_fragment_staticos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a12_fragment_staticos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

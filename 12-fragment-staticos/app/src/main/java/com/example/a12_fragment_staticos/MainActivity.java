1c8004d202b4574f5d161e66820247f92f7ac302
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-29 17:57:55 -0500

    Fragments - Estáticos
    Para crear fragments e integranlos a la interfaz de la actividad, se
    debe realizar lo siguiente.
    
    1. Crear un Fragment, dando clic derecho sobre el paquete del proyecto,
    new->fragment
    2. Se crea una clase que extiende se Fragmet y un archivo xml
    vinculada a la clase creada. El wizard de Android Studio crea todo
    3. Crear el diseño del fragment, este se debe hacer, dentro de la
    etiqueta FrameLayout
    4. Vincularlos en la actividad principal con la etiqueta fragment, y
    colocarle los atributos de id y name para hacer referencia a la clase
    que lo controla y layout, para indicarle cual es el diseño del fragment
    
    Luego de hacer eso, con x cantidad de fragments, se debe implementar el
    método OnFragmentInteractionListener a cada uno de los fragments creados
    en la actividad que los vaya  usar, de no hacer eso se producirán
    errores y no se ejecutará la aplicación

--- 12-fragment-staticos/app/src/main/java/com/example/a12_fragment_staticos/MainActivity.java
+++ 12-fragment-staticos/app/src/main/java/com/example/a12_fragment_staticos/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.a12_fragment_staticos;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements One.OnFragmentInteractionListener, Two.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }

a46d5c85d4f8798605900f1a3e2ee210519d7850
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-30 16:11:31 -0500

    Agregar Sonido a un Botón en Android
    Para agregar sonidos a Android usamos la clase MediaPlayer para la
    reproducción de los sonidos.
    
    1. Creamos un directorio de recursos "raw", y dentro de él incluimos el
    audio
    2. Instanciamos la clase MediaPlayer
    3. Agregarmos el recursos al objeto creado
    4. Al hacer clic en el botón, reproducimos el audio, con la función
    start() de la clase MediaPlayer.

--- 11-animar-boton-personalizado/app/src/main/java/com/example/a11_animar_boton_personalizado/MainActivity.java
+++ 11-animar-boton-personalizado/app/src/main/java/com/example/a11_animar_boton_personalizado/MainActivity.java
@@ -1,19 +1,36 @@
 package com.example.a11_animar_boton_personalizado;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    /*Intanciamos el objto MediaPlayer*/
+    MediaPlayer mediaPlayer;
+    Button btnSaludar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Agregarmos el sonido desde indicandole el contexto y el recurso*/
+        mediaPlayer = MediaPlayer.create(this, R.raw.clic);
+
+        btnSaludar = (Button) findViewById(R.id.btnSaludar);
+
+        btnSaludar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                Toast.makeText(getApplicationContext(), "Hola, ¿Cómo estas?", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
-    public void saludar(View view) {
-        Toast.makeText(getApplicationContext(), "Hola, ¿Como estas?", Toast.LENGTH_SHORT).show();
-    }
 }

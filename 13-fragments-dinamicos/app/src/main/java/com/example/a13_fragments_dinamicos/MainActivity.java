68279bc69fccbaa8e5f0c50de8ad631923effb89
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-29 19:23:36 -0500

    Fragmentos - Transacciones de fragmentos
    Una excelente caracterisica de los fragmentos, es la capacidad de
    agregar, quitar, reemplazar y reaalizar otras acciones en respuesta a
    las acciones de los usuarios.
    
    Para cambiar de un fragmento a otro en la actividad, se usa la API
    FragmentTransaction, con ella, usted puede cambiar entre diseños o
    distintas funciones que disponga a las acciones del usuario.
    Esta API, le permite guardar también cada transacción en una pila de
    activiidades administrada por la actividad, que le permite navegar hacia
    atrás entre fragments, así como se hace en las actividades. Para ello se
    usa el método addToBackStack.
    
    Para las transacciones se realiza lo siguiente
    1. Se instancia el Fragment creado (La clase)
    2. Se intancia FragmentTransaction
    3. Se reemplaza el fragment en el contendor de la vista de la actividad.
    4. Se inicia la transacción.
    
    Debe tener un contenedor en el diseño de la actividad, para que los
    fragment se reemplacen ahí.

--- 13-fragments-dinamicos/app/src/main/java/com/example/a13_fragments_dinamicos/MainActivity.java
+++ 13-fragments-dinamicos/app/src/main/java/com/example/a13_fragments_dinamicos/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.a13_fragments_dinamicos;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    Button btnFragmentOne, btnFragmentTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFragmentOne = (Button) findViewById(R.id.btnFragmentOne);
+        btnFragmentTwo = (Button)findViewById(R.id.btnFragmentTwo);
+
+        btnFragmentOne.setOnClickListener(this);
+        btnFragmentTwo.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.btnFragmentOne:
+                OneFragment oneFragment = new OneFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.containerFragment, oneFragment);
+                transaction.commit();
+                break;
+
+            case  R.id.btnFragmentTwo:
+                TwoFragment twoFragment = new TwoFragment();
+                FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
+                transaction1.replace(R.id.containerFragment, twoFragment);
+                transaction1.commit();
+                break;
+
+        }
+
     }
 }

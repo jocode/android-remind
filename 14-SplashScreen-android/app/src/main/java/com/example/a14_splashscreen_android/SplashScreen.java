5c94fbbd05511f8fe029e9b2348de94ca441d41e
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-05-30 19:18:15 -0500

    SplashScreen
    1. Definir la actividad del SplashScreen como la primera que se ejecuta,
    esto se define en el manifest con la etiqueta intent-filter
    2. En la actividad del splash, crear una handler, usando el método
    postDelayed, que va a tener un ejecutor y un tiempo de espera.
    Con el método postDelayed(), pasará a la siguiente actividad despues del
    tiempo que le indiquemos.
    
    Cada quien define la interfaz del splashscreen como más le convenga.
    
    Para que la actividad del splash ocupe toda la pantalla, se debe
    extender la Clase del splash de Activity en lugar de AppCompactActivity,
    de esta manera en el manifest se puede cambiar el tema a uno que sea
    FullScreen como este Theme.NoTitleBar.Fullscreen

--- 14-SplashScreen-android/app/src/main/java/com/example/a14_splashscreen_android/SplashScreen.java
+++ 14-SplashScreen-android/app/src/main/java/com/example/a14_splashscreen_android/SplashScreen.java
@@ -1,13 +1,26 @@
 package com.example.a14_splashscreen_android;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        }, 2000);
+
     }
 }

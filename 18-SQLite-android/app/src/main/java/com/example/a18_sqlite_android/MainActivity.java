70aefc069c937575562b11ee82e16c379e749370
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-06-09 14:29:10 -0500

    SQLite - Buscar datos en la base de datos
    Para buscar datos con SQLite, se debe obtener el repositorio de los
    datos de forma que se pueda leer, con el método getReadableDatabase();
    
    *  Se crea una proyección con los datos que se desean consultar
    * Se crea la seleccion con la columna que desea consultar
    * Se creael argumento de Selección para realizar la consulta
    * Se indica el orden de la consulta, si se desea
    
    Luego, se crea un cursor donde usamos el método query del objeto
    SQLiteDatabase y se le pasa por parámetro lo mencionado anteriormente
    
    * Movemos el cursos a la primera posición de los resultados con el
    método moveToFirst();
    
    Finalmente mostramos los datos en los campos de texto, usando el método
    getString() o getLong(), del objeto cursos y pasándose por parámetro la
    posición.
    
    Es importante que se haga esto dentro de un control de errores
    Try-catch, para que no se generen errores al no encontrar resultados.

--- 18-SQLite-android/app/src/main/java/com/example/a18_sqlite_android/MainActivity.java
+++ 18-SQLite-android/app/src/main/java/com/example/a18_sqlite_android/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a18_sqlite_android;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -56,12 +57,54 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+
             }
         });
 
         btnBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // Para leer información de la base de datos se define el repositorio como leible
+                SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+                // Se define una proyección con las columnas que va a consultar
+                String[] proyection = {
+                        DbHelper.TablaPersona.COLUMNA_NOMBRES,
+                        DbHelper.TablaPersona.COLUMNA_TELEFONO
+                };
+
+                // Si se desea crear una condición en la consulta por ejemplo WHERE "title" = 'Mi titulo'
+                 String selection = DbHelper.TablaPersona.COLUMNA_ID + " = ?";
+                 String[] selectionArgs = { etId.getText().toString() };
+
+
+                // Para ordenar los elementos del cursos, en caso de que sean varios datos
+                /**
+                 * String sortOrder = TablaConsulta.NombreColumna + "DESC";
+                 */
+
+                Cursor cursor = db.query(
+                        DbHelper.TablaPersona.TABLE_NAME,     // Tabla a consultar
+                        proyection,                           // Columnas a devolver
+                        selection,                            // Columna para la clausula WHERE
+                        selectionArgs,                        // Valor para para la clásula WHERE
+                        null,                                 // No agrupar las filas
+                        null,                                 // No filtrar por grupos de filas
+                        null                                  // Orden de la consulta
+                );
+
+                // moveToFirst() Coloca la "posición de lectura" en la primera entrada de los resultados
+                cursor.moveToFirst();
+
+                // Colocar los registros en los campos de texto
+                try {
+                    etNombres.setText(cursor.getString(0));
+                    etTelefono.setText(cursor.getString(1));
+                } catch (Exception exeption){
+                    Toast.makeText(getApplicationContext(), "No hay Información para la consulta", Toast.LENGTH_LONG).show();
+                }
+
 
             }
         });

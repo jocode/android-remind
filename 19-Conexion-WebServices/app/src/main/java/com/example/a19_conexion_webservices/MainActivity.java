b09943b23fe1de7d82667f5f898add4922acdc78
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-06-12 16:36:15 -0500

    WebServices - Librería HttpsURLConnection
    Es importante resaltar que es recomendable usar la conexión y carga de
    datos en hilos separados. Esto se hace usando una clase que extienda de
    una Tarea Asíncrona, de esta manera no se verán lags o retrasos en la
    interfaz de usuario.
    
    1. Debemos agregar en el manifest los permisos de Internet y estado de
    red
    2. Crear el método downloadURL(), que hará todo el proceso de conexión
    al servicio web
    3. Crear la clase que se usará para cada método, estas clases deben de
    extender de AsyncTask para evitar afectar la interfaz de usuario.

--- 19-Conexion-WebServices/app/src/main/java/com/example/a19_conexion_webservices/MainActivity.java
+++ 19-Conexion-WebServices/app/src/main/java/com/example/a19_conexion_webservices/MainActivity.java
@@ -1,13 +1,150 @@
 package com.example.a19_conexion_webservices;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGuardar, btnConsultar;
+    EditText etIdUser, etNombres, etTelefono;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etIdUser = (EditText) findViewById(R.id.etIdUser);
+        etNombres = (EditText) findViewById(R.id.etNombres);
+        etTelefono = (EditText) findViewById(R.id.etTelefono);
+
+        btnGuardar = (Button)  findViewById(R.id.btnGuardar);
+        btnConsultar = (Button) findViewById(R.id.btnConsultar);
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new GuardarDatos().execute("http://192.168.0.102/webServices/registro.php?nombres="+etNombres.getText().toString()+"&telefono="+etTelefono.getText().toString());
+            }
+        });
+
+        btnConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ConsultarDatos().execute("http://192.168.0.102/webServices/consulta.php?id="+etIdUser.getText().toString()) ;
+            }
+        });
     }
+
+
+    private class GuardarDatos extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+            Toast.makeText(getApplicationContext(), "Se almacenaron los datos correctamente", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private class ConsultarDatos extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+            JSONArray jsonArray = null;
+            try {
+                jsonArray = new JSONArray(result);
+                etNombres.setText(jsonArray.getString(1));
+                etTelefono.setText(jsonArray.getString(2));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        Log.i("URL",""+myurl);
+        // Reemplazar los espacios en blanco por %20
+        myurl = myurl.replace(" ","%20");
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("respuesta", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+
 }
+
+

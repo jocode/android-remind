99c72a461e2651fe7e093fc1da21a4f2efa9aee6
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-06-15 13:40:07 -0500

    Volley - Enviar una petición Simple
    Volley se encarga de ejecutar todas nuestras peticiones en un hilo
    separado de nuestra UI y, además crea una fila de peticiones para no
    sobrecargar el trabajo de la conexión.
    
    Para usar volley debemos:
    1. Agregar la librería a las dependencias de Gradle
    2. Agregar los permisos de Internet a nuestra aplicación en el
    Manifiesto
    3. Usar el RequestQueue, para usar la lista de peticiones que se harán
    4. Agregar la url donde se va a hacer la petición
    Crear un StringRequest donde le indicamos el método HTTP para la
    obtención de datos, la url donde se hará la petición y los métodos que
    nos mostrarán la respuesta obtenida, bien sea satisfactoria o con
    errores.
    5. Agregar el StringRequest a RequestQueue

--- 21-Volley/app/src/main/java/com/example/a21_volley/MainActivity.java
+++ 21-Volley/app/src/main/java/com/example/a21_volley/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.a21_volley;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tvResponse = (TextView) findViewById(R.id.tvResponse);
+
+        // Instanciamos el RequestQueue
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        //Url a la cual hacemos la peticion
+        String url = "http://192.168.0.102/volleyServices/getUser.php?cc=12345";
+
+        // Solicita una respuesta de String de la url propporcionada
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                //Respuesta recibida
+                tvResponse.setText("Respuesta: "+response);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                //Error en la repuesta
+                tvResponse.setText("Error: "+error);
+
+            }
+        });
+
+        // Agregamos la respuesta al RequestQueue
+        queue.add(stringRequest);
+
+
     }
 }

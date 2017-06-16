7087b66a9ec52c2bcea9d65ab46a97675827a437
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-06-15 19:59:58 -0500

    Sugar - Listar todos los datos en un ListView
    Para consultar todos los datos con Sugar, usamos el método ListAll y de
    esta manera automáticamente el ORM nos consultará todos los datos.
    
    Para ponerlos en un listView, creamos un ArrayList y recorremos todos
    los elementos de la lista Persona que hemos consultado. Ya en cada
    iteración obtenemos del Objeto personas los atributos que queremos
    mostrar, y finalmente creamos el adaptador y le pasamos el Listado de
    los elementos y al listView le definimos el adaptador.

--- 22-Sugar-ORM-for-SQLite/app/src/main/java/com/example/a22_sugar_orm_for_sqlite/MainActivity.java
+++ 22-Sugar-ORM-for-SQLite/app/src/main/java/com/example/a22_sugar_orm_for_sqlite/MainActivity.java
@@ -4,16 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etIdentificacion, etNombres, etApellidos, etTelefono;
     Button btnGuardar, btnConsultar;
+    ListView lvResulado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
         btnGuardar = (Button) findViewById(R.id.btnGuardar);
         btnConsultar = (Button) findViewById(R.id.btnConsultar);
+
+        lvResulado = (ListView) findViewById(R.id.lvResultados);
 
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +59,10 @@ public class MainActivity extends AppCompatActivity {
                     etNombres.setText(persona.getNombres());
                     etApellidos.setText(persona.getApellidos());
                     etTelefono.setText(persona.getTelefono());
+
+                    // Cargar todos los datos
+                    loadResultSQLite();
+
                 } catch (Exception exception){
                     Toast.makeText(getApplicationContext(), "No se encontraron datos para la búsqueda", Toast.LENGTH_LONG).show();
                 }
@@ -60,5 +70,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void loadResultSQLite() {
+        // Hacer la consulta de todos los datos de la Clase Persona
+        List<Persona> personas = Persona.listAll(Persona.class);
+        // Crear un array list para agregar los datos al ListView
+        ArrayList<String> listado = new ArrayList<String>();
+
+        // Recorrer el tamaño de los datos de la consulta y agregarlo al ArrayList
+        for (int i = 0; i < personas.size(); i++){
+            Persona persona =  personas.get(i);
+            listado.add(""+persona.getId()+" - "+persona.getIdentificacion()+" - "+persona.getNombres() );
+        }
+
+        // Definir el Adaptador, y pasarle el listado
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listado);
+
+        // Definir el adaptador al ListView
+        lvResulado.setAdapter(adapter);
     }
 }
